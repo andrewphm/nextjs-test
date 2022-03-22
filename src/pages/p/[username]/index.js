7aec9f771e6f2634d423452310376ddacdf1ae46
andrewphm <andrpham@gmail.com>
2022-03-22 13:08:49 -0400

    render components

--- src/pages/p/[username]/index.js
+++ src/pages/p/[username]/index.js
@@ -2,6 +2,10 @@ import dbConnect from '../../../lib/dbConnect';
 import mongoose from 'mongoose';
 import Post from '../../../models/Post';
 import User from '../../../models/User';
+import Link from 'next/link';
+import ProfileContent from '../../../components/profile/ProfileContent';
+import ProfileInfo from '../../../components/profile/ProfileInfo';
+import Layout from '../../../components/layouts/Layout';
 
 export async function getServerSideProps(context) {
   await dbConnect();
@@ -41,11 +45,45 @@ export async function getServerSideProps(context) {
 
 export default function Profile({ userData, userPosts }) {
   // If user cannot be found.
+  if (!userData) {
+    return (
+      <Layout>
+        <div className="my-10 mx-auto flex flex-col gap-y-5 px-10 text-center">
+          <p className="text-2xl font-semibold">
+            Sorry, this page is not available.
+          </p>
+
+          <p>
+            The link you followed may be broken, or the page may have been
+            removed.{' '}
+            <Link href="/">
+              <a>
+                <span className="cursor-pointer text-blue-btn">
+                  Click here to go back home.
+                </span>
+              </a>
+            </Link>
+          </p>
+        </div>
+      </Layout>
+    );
+  }
+
   return (
-    <div>
-      <p>{JSON.stringify(userData)}</p>
-      <br></br>
-      <p>{JSON.stringify(userPosts)}</p>
-    </div>
+    <Layout>
+      <section className="min-h-[80vh]">
+        <ProfileInfo userData={userData} userPosts={userPosts} />
+        {userData.isPrivate ? (
+          <div className="mx-auto flex w-full flex-col border-y border-neutral-300  bg-white md:max-w-4xl">
+            <div className="mx-auto flex flex-col py-10 text-center">
+              <p className="text-sm font-semibold">This Account is Private</p>
+              <p className="text-sm">Follow to see their photos and videos.</p>
+            </div>
+          </div>
+        ) : (
+          <ProfileContent userPosts={userPosts} />
+        )}
+      </section>
+    </Layout>
   );
 }
