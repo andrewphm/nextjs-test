6276acc5bcb38ae5019a421531434d06718e884a
andrewphm <andrpham@gmail.com>
2022-03-21 20:16:59 -0400

    SSR test

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const PostCommentSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const PostSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    caption: {
+      type: String,
+      default: '',
+    },
+    comments: [PostCommentSchema],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Post || mongoose.model('Post', PostSchema);
